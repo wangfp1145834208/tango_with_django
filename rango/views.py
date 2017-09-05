f65e403d19aade5c0c986eaa467dadaf675d5840
wangfp <wangfp_nk@163.com>
2017-09-06 00:43:47 +0800

    excise 1

--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
-    return HttpResponse("Rango is going on!")
+    return HttpResponse("Rango is going on!<br/>"
+                        "<a href='/rango/about'>About</a>")
+
+
+def about(request):
+    return HttpResponse("Rango's about page<br/>"
+                        "<a href='/rango'>Index</a>")
