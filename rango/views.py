cfbfe7e7ff29b51d851f43a2158ef2b4d19b7995
wangfp <wangfp_nk@163.com>
2017-09-09 00:28:48 +0800

    djano.contrib.auth

--- rango/views.py
+++ rango/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Category, Page
-from .forms import CategoryForm, PageForm
+from .forms import CategoryForm, PageForm, UserForm, UserProfileForm
 
 
 def index(request):
@@ -65,6 +65,35 @@ def add_page(request, category_name_slug):
 
     context_list = {'form': form, 'category': cat}
     return render(request, 'rango/add_page.html', context=context_list)
+
+
+def register(request):
+    user_form = UserForm()
+    profile_form = UserProfileForm()
+    registered = False
+
+    if request.method == 'POST':
+        user_form = UserForm(request.POST)
+        profile_form = UserProfileForm(request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+            profile.save()
+
+            registered = True
+        else:
+            print(user_form.errors, profile_form.errors)
+
+    return render(request, 'rango/register.html', {'user_form': user_form,
+                                                   'profile_form': profile_form,
+                                                   'registered': registered})
 
 
 
