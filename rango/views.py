6870c7f161c075d1bd5545ad9ca30638c73ebd45
wangfp <wangfp_nk@163.com>
2017-09-10 00:08:41 +0800

    cookie and session

--- rango/views.py
+++ rango/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import authenticate, login, logout
@@ -8,12 +10,36 @@ from .models import Category, Page
 from .forms import CategoryForm, PageForm, UserForm, UserProfileForm
 
 
+def vistor_cookie_handler(request, response):
+    visits = int(request.COOKIES.get('visits', '1'))
+
+    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+                                        '%Y-%m-%d %H:%M:%S')
+
+    if (datetime.now() - last_visit_time).seconds > 0:
+        visits += 1
+        response.set_cookie('last_visit', str(datetime.now()))
+    else:
+        response.set_cookie('last_visit', last_visit_cookie)
+
+    response.set_cookie('visits', visits)
+
+
 def index(request):
+    # request.session.set_test_cookie()
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
+    visits = request.COOKIES.get('visits', '1')
     context = {'categories': category_list,
-               'pages': page_list}
-    return render(request, 'rango/index.html', context=context)
+               'pages': page_list,
+               'visits': visits,
+               }
+
+    response = render(request, 'rango/index.html', context=context)
+    vistor_cookie_handler(request, response)
+
+    return response
 
 
 def show_category(request, category_name_slug):
@@ -136,6 +162,9 @@ def restricted(request):
 
 
 def about(request):
+    # if request.session.test_cookie_worked():
+    #     print('TEST COOKIE WORKED!')
+    #     request.session.delete_test_cookie()
     print(request.method)
     print(request.user)
     context = {'creator': 'wangfp'}
